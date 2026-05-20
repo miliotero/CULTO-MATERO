f009dce95e8b0e03f19741f8776f1b3c7de5864b
miliotero <mmiliotero@gmail.com>
2026-05-20 12:03:49 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -23,9 +23,7 @@ const combos = [
     name: "Combo Clásico",
     price: "$35.000",
     description: "
-      Mate camionero algarrobo laqueado 
-      Bombilla pico de loro 
-      Yerbero gamuzado",
+      Mate camionero algarrobo laqueado + Bombilla pico de loro + Yerbero gamuzado",
     image: "/combo_clasico.png",
   },
   {
